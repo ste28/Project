7653306c6358152bf19735e13b4457874df96c8e
stefanus santoso <stefanus2800@gmail.com>
2021-06-12 12:00:55 +0700

    tambah class lib namun masih error

---
+++ Data/DataDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Data_DB
+{
+    public static class DataDB
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost\\SQLExpress;Initial Catalog=Project UAS;Persist Security Info=True;User ID=sa;Password=***********";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
--- Project PCS/Form1.cs
+++ Project PCS/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Data_DB;
 
 namespace Project_PCS
 {
